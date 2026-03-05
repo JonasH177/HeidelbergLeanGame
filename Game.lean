12e5a1bef47224886b8e0dc2308da7c727de7ab5
Florent Schaffhauser <florent.schaffhauser@gmail.com>
2026-03-05 17:43:12 +0100

    Update list of contributors and maintainers

--- Game.lean
+++ Game.lean
@@ -19,6 +19,7 @@ Credits:
 
 - Heide Frank
 - Johannes Kadel
+- Levi Klein
 - Adriano Messina
 - Hanna Rothe
 - Alina Stock
@@ -28,8 +29,7 @@ Credits:
 
 The current version is being developed and maintained by:
 
-- Heide Frank
-- Levi Klein
+- Jonas Heese
 - Kevin Klinge
 - Florent Schaffhauser
 "
